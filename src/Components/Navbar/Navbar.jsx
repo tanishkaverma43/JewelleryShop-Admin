2441b09315430f37d33f1d1128ff2056e3cfb1e1
Tanishka Verma <tanishkaverma43@gmail.com>
2025-05-03 11:18:52 +0530

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../Assets/nav-logo.svg'
+import navprofileIcon from '../Assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h2>SHINECART</h2>
+    </div>
+  )
+}
+
+export default Navbar
